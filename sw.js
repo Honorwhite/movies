d3e01bdd12f245872fe4e6fa855d1c581972623e
Honorwhite <1onurakbas@gmail.com>
2026-01-04 19:39:10 +0300

    ikon

--- sw.js
+++ sw.js
@@ -4,8 +4,13 @@ const ASSETS = [
     './index.html',
     './style.css',
     './script.js',
-    './icon.png',
-    './manifest.json'
+    './manifest.json',
+    './appimages/android/android-launchericon-48-48.png',
+    './appimages/android/android-launchericon-72-72.png',
+    './appimages/android/android-launchericon-96-96.png',
+    './appimages/android/android-launchericon-144-144.png',
+    './appimages/android/android-launchericon-192-192.png',
+    './appimages/android/android-launchericon-512-512.png'
 ];
 
 self.addEventListener('install', (event) => {
